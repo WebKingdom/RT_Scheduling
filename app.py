b29337fd7e2c3a579f4502d644b4befbdab4c164
SomaSz <soma.szabo15@gmail.com>
2022-11-22 04:42:21 -0600

    Added schedulability checks

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import plotly.express as px
 import plotly.figure_factory as ff
 import pandas as pd
+import math
 
 # df = pd.DataFrame([
 #     dict(Task="Job A", Start=0, Finish=2),
@@ -15,7 +16,7 @@ DISABLED = 0
 LOW = 1
 MEDIUM = 2
 HIGH = 3
-DEBUG_LEVEL = LOW
+DEBUG_LEVEL = HIGH
 
 class TimeFrame:
     def __init__(self, start_t=0, end_t=0):
@@ -41,6 +42,60 @@ class Task:
 
     def __str__(self):
         return f"Task {self.name}: ({self.exec_t}, {self.period}). TimeFrames: {self.time_frames}"
+
+# RMS utilization test returns true if task set is schedulable, false otherwise (sufficient but not necessary)
+def rms_utilization_test(tasks) -> bool:
+    n = len(tasks)
+    limit = float(n * (2.0 ** (1.0 / n) - 1.0))
+    sum = 0.0
+    for task in tasks:
+        sum += float(task.exec_t) / float(task.period)
+    return sum <= limit
+
+# RMS exact analysis test (completion time test) return true if task set is schedulable, false otherwise
+def rms_exact_analysis_test(tasks) -> bool:
+    # iterate over tasks from lowest to highest priority (period) and workload will only include subset of tasks that have not passed the test yet
+    for i in range(len(tasks) - 1, -1, -1):
+        task = tasks[i]
+        cur_time = 0.0
+        workload = compute_workload(tasks[0:i+1], cur_time, initial_workload=True)
+        while((cur_time != workload) and (workload <= task.period)):
+            cur_time = workload
+            workload = compute_workload(tasks[0:i+1], cur_time)
+        
+        if (cur_time == workload):
+            if DEBUG_LEVEL >= MEDIUM: print(str(task) + " schedulable by exact analysis due to: " + str(cur_time) + " == " + str(workload))
+        elif (workload > task.period):
+            if DEBUG_LEVEL >= MEDIUM: print(str(task) + " NOT schedulable by exact analysis due to: " + str(workload) + " > " + str(task.period))
+            return False
+    return True
+
+def compute_workload(tasks, cur_time, initial_workload=False) -> float:
+    workload = 0.0
+    for task in tasks:
+        if (initial_workload):
+            workload += float(task.exec_t)
+        else:
+            workload += float(task.exec_t) * float(math.ceil(float(cur_time) / float(task.period)))
+    return workload
+
+# EDF utilization test return true if task set is shedulable, false otherwise
+def edf_utilization_test(tasks) -> bool:
+    sum = 0.0
+    for task in tasks:
+        sum += float(task.exec_t) / float(task.period)
+    return sum <= 1.0
+
+# get tasks and sort from high to low priority
+# tasks = [Task("T1", 1, 8), Task("T2", 2, 6), Task("T3", 4, 24)]
+# tasks = [Task("T1", 3, 12), Task("T2", 3, 12), Task("T3", 8, 16)]
+tasks = [Task("T1", 2, 8), Task("T2", 3, 12), Task("T3", 4, 16)]
+
+tasks = sorted(tasks, key=lambda task: task.period)
+
+print("RMS utilization test: " + str(rms_utilization_test(tasks)))
+print("RMS exact analysis test: " + str(rms_exact_analysis_test(tasks)))
+print("EDF utilization test: " + str(edf_utilization_test(tasks)))
 
 
 # A = (2, 10), B = (1, 15) where (ci, pi)
